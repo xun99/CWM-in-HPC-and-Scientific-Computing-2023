3ba68f5236b04211740063fdf6c10ebe5ff1604f
Karel Adámek <karel.adamek@gmail.com>
2023-06-13 14:15:05 +0100

    Fixed sbatch scripts for prac4

--- Practicals/prac4/code/integral/run_integral_omp.sh
+++ Practicals/prac4/code/integral/run_integral_omp.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
-
+# set the number of nodes and processes per node
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=16
-#SBATCH --time=00:05:00
+#SBATCH --partition=short
+
+# set max wallclock time
+#SBATCH --time=00:10:00
+#SBATCH --gres=gpu:1
+
+# set name of job
 #SBATCH --job-name=integral
+
+# Use our reservation
+#SBATCH --reservation=training
+
 
 module purge
 module load gcc/4.9.2
